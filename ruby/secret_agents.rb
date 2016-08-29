2063afcf40982511356b01ef792e64cdb5f2bb88
Sam Hopkins <hopkins.571@gmail.com>
2016-08-29 01:41:06 -0500

    Rel 5

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -51,10 +51,36 @@ def decrypt(y)
 	end
 	p output
 end
-encrypt("abc") 
-encrypt("zed") 
-decrypt("bcd") 
-decrypt("afe")
-decrypt(encrypt("swordfish"))
+# Ask user if they want to encrypt or decypt
+# Ask user for input (password)
+# Need conditional based on input
+# if encrypt, print output of encrypt method on user's input
+# if decrypt, print output of decrypt method on user's input
+validresponse = false
+until validresponse == true
+puts "Welcome, agent."
+puts "Would you like to encrypt or decrypt a password?"
+puts "Enter 1 to encrypt or 2 to decrypt."
+action = gets.chomp
+	if action == "1"
+		puts "Please enter the password to encrypt."
+		password = gets.chomp
+		encrypt(password)
+		validresponse = true
+	elsif action == "2"
+		puts "Please enter the password to decrypt."
+		password = gets.chomp
+		decrypt(password)
+		validresponse = true
+	else
+		puts "That response was invalid. Please try again."
+	end
+end
+
+# encrypt("abc") 
+# encrypt("zed") 
+# decrypt("bcd") 
+# decrypt("afe")
+# decrypt(encrypt("swordfish"))
 # The nested method call works because the encrypt method returns a valid string that can be used as an argument for the decrypt method. 
 # Furthermore, because the decrypt method was defined outside of the encrypt method both methods could be run within the same line of code.
