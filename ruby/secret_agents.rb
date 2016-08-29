62398ac938e71eef1c0379d8d4ca8c223b95b6f3
Sam Hopkins <hopkins.571@gmail.com>
2016-08-29 01:04:24 -0500

    Added pseudocode

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,25 @@
+# Encrypt Method Pseudocode
+# 
+# Define a method that takes a string of text and advances each character of the string index by index one letter forward.
+# Create a parameter that is a string of text with parameter name 'x'
+# Define variable 'output' as a new string and a new variable 'index' equal to 0.
+# Build a loop until index equals length of the string.
+# Add to the variable output the next value of the index of 'x.'
+# Add 1 to index.
+# End the loop.
+# Print variable output.
+#
+# Decrypt Method Pseudocode
+#
+# Define a method that takes a string of text and reverses each character by one letter in the alphabet.
+# Define 'alphabet' variable that is a string of all letters in the alphabet.
+# Define variable 'output' as a new string and a new variable 'index' equal to 0.
+# Build a loop until index equals length of the string.
+# Add to the variable output the following:
+# The value of the following index of the variable alphabet
+# 1 less than the index of alphabet at the following character within the string 'alphabet.'
+# The index of 'y' at the variable index.
+# Add 1 to index.
+# End the loop.
+# Print variable output.
+
