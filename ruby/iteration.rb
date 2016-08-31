2ab826df6d0a50ffaab935a123b0cec0c7497c2f
Sam Hopkins <hopkins.571@gmail.com>
2016-08-30 21:54:10 -0500

    Release 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -34,3 +34,67 @@ end
 
 # Print results
 puts array
+
+# Doing the last thing
+# Number one - array
+
+array.delete_if {|color| color == "red-ish"}
+
+puts "after deleting one:"
+puts array
+
+# Number one - hash
+puts "Before:"
+puts hash
+hash.delete_if {|name, rbi| rbi < 300}
+puts "After:"
+puts hash
+
+# Number two - array
+puts "Before:"
+puts array
+array.keep_if {|color| color == "black-ish"}
+puts "after:"
+puts array
+
+hash["Trump"] = 245
+hash["Warren"] = 245
+# Number two - hash
+puts "Before:"
+puts hash
+hash.keep_if {|name, rbi| rbi > 300}
+puts "After:"
+puts hash
+
+# Number three - array
+array << "blue-ish"
+array << "green"
+array << "tangerine"
+puts "Before"
+puts array
+new_array = array.reject {|color| color == "blue-ish"}
+puts "After"
+puts new_array
+
+# Number three - hash
+hash["Trump"] = 245
+hash["Warren"] = 245
+puts "Before"
+puts hash
+new_hash = hash.select {|name, rbi| rbi > 300}
+puts "After"
+puts new_hash
+
+# Number four - array
+puts "Before"
+puts array
+new_array = array.drop_while {|color| color.include?("-ish")}
+puts "After"
+puts new_array
+
+# Number four - hash
+puts "Before"
+puts hash
+new_hash = hash.reject {|name, rbi| rbi < 300}
+puts "After"
+puts new_hash
