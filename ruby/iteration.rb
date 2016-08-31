5384025286c8ee65af5541dbfb54a32b840c45de
Sam Hopkins <hopkins.571@gmail.com>
2016-08-30 21:11:24 -0500

    Release 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -4,3 +4,33 @@ def hello(x,y)
 end
 
 hello("Chuck","Chicago") {|a, b| puts a+b}
+
+
+array = ["blue", "yellow", "red", "orange", "purple", "black"]
+hash = {
+	"Nader" => 251,
+	"Clinton" => 437,
+	"Gore" => 257,
+	"Kaine" => 537,
+	"Trump" => 193
+}
+
+array.each do |color|
+	puts "#{color} is a color. Cool.".capitalize
+end
+
+hash.each do |name, rbi|
+	puts "#{name} is currently batting #{rbi}."
+end
+
+# Print array before modification
+puts array
+
+# Modify that array
+
+array.map! do |color|
+	color.insert(-1,"-ish")
+end
+
+# Print results
+puts array
