7b1fbc4310dff890ab24cfd230a1a5b47118dc76
Sam Hopkins <hopkins.571@gmail.com>
2016-08-31 19:32:01 -0500

    Added inputs - theme and dwelling area

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -29,3 +29,21 @@ until valid_kids == true
 		valid_kids = true
 	end
 end
+# Extra string input for decor theme
+puts "Please enter the decor theme."
+form[:decor] = gets.chomp
+# Input for area of of residence - convert to integer, nil if unknown
+puts "How many square feet in the residence? Enter unknown if unknown."
+valid_area = false
+until valid_area == true
+	form[:area] = gets.chomp
+	if form[:area].to_i < 0
+		puts "I'm sorry. That response was invalid."
+	elsif form[:area].downcase == "unknown"
+		form[:area] = nil
+		valid_area = true
+	else
+		form[:area] = form[:area].to_i
+		valid_area = true
+	end
+end
