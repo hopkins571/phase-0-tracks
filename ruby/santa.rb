a83974ac4adbfab21cb1d5386d2525cf7bf7132e
Sam Hopkins <hopkins.571@gmail.com>
2016-09-07 21:02:54 -0500

    Release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -15,64 +15,108 @@ class Santa
 	def print_demos
 		puts "Age is #{@age}, ethnicity is #{@ethnicity}, gender is #{@gender}."
 	end
-	def add1_to_age
+	def celebrate_birthday
 		@age += 1
 	end
-	def age_randomizer
+	def get_mad_at(scapegoat)
+		if @reindeer_ranking.include?(scapegoat)
+			@reindeer_ranking.delete_if {|reindeer| reindeer == scapegoat}
+			@reindeer_ranking.push(scapegoat)
+		end
+		@reindeer_ranking
+	end
+# gender setter method	
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+# age getter method	
+	def age
+		@age
+	end
+# ethnicity getter method
+	def ethnicity
+		@ethnicity
+	end
+	def least_fave_reindeer
+		@reindeer_ranking[-1]
+	end
+	def random_age
 		@age = (1..100).to_a.sample
 	end
 end
+
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+billybob = Santa.new(example_genders.sample,example_ethnicities.sample)
+puts billybob.print_demos
+billybob.celebrate_birthday
+puts billybob.print_demos
+puts billybob.age
+billybob.gender = "genderqueer"
+puts billybob.print_demos
+puts billybob.least_fave_reindeer
+billybob.get_mad_at("Vixen")
+puts billybob.least_fave_reindeer
+billybob.random_age
+puts billybob.age
+puts billybob.ethnicity
+
+# groovy_santas = []
+# 5.times do |i|
+# 	groovy_santas << Santa.new(example_genders.sample,example_ethnicities.sample)
+
+
 
 # st_nick = Santa.new
 # st_nick.speak
 # st_nick.eat_milk_and_cookies("generic Oreo")
 
-santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
-puts santas
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+# puts santas
 
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
-santas.each do |i|
-	i.print_demos
-end
-santas.each do |i|
-	i.add1_to_age
-end
-santas.each do |i|
-	i.print_demos
-end
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+# santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+# santas.each do |i|
+# i.print_demos
+# end
+# santas.each do |i|
+# i.add1_to_age
+# end
+# santas.each do |i|
+# i.print_demos
+# end
 
-random1 = Santa.new(example_genders.sample,example_ethnicities.sample)
-random1.print_demos
-random1.add1_to_age
-random1.print_demos
-random2 = Santa.new(example_genders.sample,example_ethnicities.sample)
-random2.print_demos
+# random1 = Santa.new(example_genders.sample,example_ethnicities.sample)
+# random1.print_demos
+# random1.add1_to_age
+# random1.print_demos
+# random2 = Santa.new(example_genders.sample,example_ethnicities.sample)
+# random2.print_demos
 
-santas = []
-12.times do |i|
-	santas << Santa.new(example_genders.sample,example_ethnicities.sample)
-end
-santas.each do |i|
-	i.print_demos
-end
-santas.each do |i|
-	i.age_randomizer
-end
-puts "ages are now random:"
-santas.each do |i|
-	i.print_demos
-end
+# santas = []
+# 12.times do |i|
+# santas << Santa.new(example_genders.sample,example_ethnicities.sample)
+# end
+# santas.each do |i|
+# i.print_demos
+# end
+# santas.each do |i|
+# i.age_randomizer
+# end
+# puts "ages are now random:"
+# santas.each do |i|
+# i.print_demos
+# end
 
 
