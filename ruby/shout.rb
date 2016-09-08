6b3c7cf082e995f6fb6f2b753eacd039a4c7336c
Sam Hopkins <hopkins.571@gmail.com>
2016-09-08 14:25:15 -0500

    Release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,36 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+# 	def self.yelling_happily(words)
+# 		":D :D :D " + words + "!!!!! *gleefully prances*"
+# 	end
+# end
+
+# # driver code
+# puts Shout.yell_angrily("boo hiss")
+# puts Shout.yelling_happily("free bikes for everyone")
+
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + " :("
 	end
-	def self.yelling_happily(words)
+	def yelling_happily(words)
 		":D :D :D " + words + "!!!!! *gleefully prances*"
 	end
 end
 
-# driver code
-puts Shout.yell_angrily("boo hiss")
-puts Shout.yelling_happily("free bikes for everyone")
+class Donald
+	include Shout
+end
+
+class Toddler
+	include Shout
+end
+
+toddler = Toddler.new
+donald = Donald.new
+puts toddler.yell_angrily("MINE!")
+puts donald.yell_angrily("Goo goo gah gah")
+puts toddler.yelling_happily("MOMMY!")
+puts toddler.yelling_happily("I've got nothing for this one")
