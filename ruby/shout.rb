0e66a6d715cc6f655ee78718fb12b4a2856fae8f
Sam Hopkins <hopkins.571@gmail.com>
2016-09-08 14:12:52 -0500

    Release 1

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
-	# we'll put some methods here soon, but this code is fine for now!
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
+	def self.yelling_happily(words)
+		":D :D :D " + words + "!!!!! *gleefully prances*"
+	end
 end
+
+# driver code
+puts Shout.yell_angrily("boo hiss")
+puts Shout.yelling_happily("free bikes for everyone")
