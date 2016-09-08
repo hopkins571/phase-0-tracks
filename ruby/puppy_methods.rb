e6ed3cc06b5c9fa3b67dcb72665e53b9a60e2845
Sam Hopkins <hopkins.571@gmail.com>
2016-09-07 23:34:47 -0500

    created kitten class with driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,27 +1,54 @@
-class Puppy
-	def fetch(toy)
-		puts "I brought back the #{toy}!"
-    	toy
-  	end
-	def speak(i)
-		i.times {puts "Woof!"}
+# class Puppy
+# 	def initialize
+# 		puts "Initialize new puppy instance..."
+# 	end
+# 	def fetch(toy)
+# 		puts "I brought back the #{toy}!"
+#     	toy
+#   	end
+# 	def speak(i)
+# 		i.times {puts "Woof!"}
+# 	end
+# 	def roll_over
+# 		puts "*rolls over*"
+# 	end
+# 	def dog_years(i)
+# 		i * 7
+# 	end
+# 	def play_dead
+# 		puts "Puppy is dead. :("
+# 		puts "Just kidding! ;)"
+# 	end
+# end
+
+# driver code
+# fido = Puppy.new
+# fido.fetch("ball")
+# fido.speak(4)
+# fido.roll_over
+# puts fido.dog_years(4)
+# fido.play_dead
+
+class Kitten
+	def initialize
+		puts "Meow meow! Annyong!"
 	end
-	def roll_over
-		puts "*rolls over*"
+	def hunt_bugs(hour)
+		puts "Kitty was hunting for #{hour} hours and caught #{hour*10} bugs!"
 	end
-	def dog_years(i)
-		i * 7
-	end
-	def play_dead
-		puts "Puppy is dead. :("
-		puts "Just kidding! ;)"
+	def litterbox(is_clean)
+		if is_clean
+			puts "Kitty used the litter box!"
+		else
+			puts "Kitty pooped outside the box!"
+		end
 	end
 end
 
-# driver code
-fido = Puppy.new
-fido.fetch("ball")
-fido.speak(4)
-fido.roll_over
-puts fido.dog_years(4)
-fido.play_dead
+mr_meows = Kitten.new
+mr_meows.litterbox(true)
+mr_meows.hunt_bugs(4)
+mr_meows.litterbox(false)
+
+
+
