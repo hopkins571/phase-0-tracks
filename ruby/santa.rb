56e415f55db388ddc729800d06fab5a0f20746c4
Sam Hopkins <hopkins.571@gmail.com>
2016-09-07 19:44:35 -0500

    Release 0

---
+++ ruby/santa.rb
@@ -0,0 +1,16 @@
+class Santa
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance ..."
+	end
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+	def eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type}!"
+	end
+end
+
+# st_nick = Santa.new
+# st_nick.speak
+# st_nick.eat_milk_and_cookies("generic Oreo")
+
