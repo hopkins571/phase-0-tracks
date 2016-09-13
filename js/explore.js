6c2192c94d30d2171e626f24f1dfa4f86f4e081d
Sam Hopkins <hopkins.571@gmail.com>
2016-09-13 17:37:39 -0500

    pseudocoded method

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// create new function that accepts one string - variable text
+// create new variable newstring - blank string
+// create for loop - conditions:
+// define variable i = length of text minus one
+// run loop while i is >= 0
+// i is decreasing by 1 each iteration
+// loop behavior: add the character of text at index i to newstring
+// function returns newstring
+
