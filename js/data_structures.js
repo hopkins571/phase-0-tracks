90e4244ff8687b17c05ccdb8ec4cbee7a5d0f9c8
Sam Hopkins <hopkins.571@gmail.com>
2016-09-15 13:55:00 -0500

    release 2

--- js/data_structures.js
+++ js/data_structures.js
@@ -19,3 +19,21 @@ for (var i = 0; i < names.length; i++) {
 };
 console.log(stable);
 
+function Car(make,model,year,color,condition,features) {
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.color = color;
+	this.condition = condition;
+	this.features = features;
+	this.describe = function() { 
+		console.log("I am a " + this.condition + " " + this.color + " " + this.year + " " + this.make + " " + this.model + "!");
+		console.log("My features are: " + this.features.toString(', '));
+		console.log("Vroom vroom!")};
+};
+var barry = new Car("Toyota","Prius","2013","aquapurple","used",["A/C","stereo","cruise control"]);
+barry.describe();
+var sandy = new Car("Suzuki","Baleno","1999","red","used and abused",["anti-theft system","heated seats"]);
+sandy.describe();
+var lemon = new Car("Fiat","500","2016","green","new",["barely passed safety inspection"]);
+lemon.describe();
