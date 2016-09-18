18c70d0db64ddbb09696a7d99746ea04c0fe17cb
Sam Hopkins <hopkins.571@gmail.com>
2016-09-17 23:55:35 -0500

    pseudocode compare objects function

--- js/algos.js
+++ js/algos.js
@@ -17,8 +17,23 @@ function returnLongest(arrayOfStrings) {
 	return longestWord;
 }
 
+// FUNCTION: compare objects
+// INPUT PARAMETERS: two objects - object1 and object2
+// OUTPUT: boolean variable hasSharedPair
+// define variable hasSharedPair - false
+// iterate through each key of object1:
+// 	IF the value of the key currently being iterated in object1 is equal to the value of the key of the same name in object2
+// 	THEN hasSharedPair is true
+// RETURN hasSharedPair
+
+
+
+
+
+
 var testArray = ["long", "longest", "etetetetetetettetetetetetetetetetetettetetetete", "etes,th.stphsoennet", "3394949494933"];
 var anotherArray = ["1", "22", "999999999", "666666", "4444", "7777777", "55555", "333", "88888888"]
 
 console.log(returnLongest(testArray));
 console.log(returnLongest(anotherArray))
+
