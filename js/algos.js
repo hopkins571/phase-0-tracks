5ab4b7945e99f2ecbed8117bfa5db9d834fb265e
Sam Hopkins <hopkins.571@gmail.com>
2016-09-17 23:49:46 -0500

    rel 0 - complete and test returnLongest function

--- js/algos.js
+++ js/algos.js
@@ -6,3 +6,19 @@
 // IF the length of the variable longestWord is LESS THAN the length of the current index of the array
 // THEN replace the value of longestWord with the current index of the array
 // RETURN longestWord
+
+function returnLongest(arrayOfStrings) {
+	var longestWord = arrayOfStrings[0];
+	for (i = 1; i <= (arrayOfStrings.length - 1); i++) {
+		if (longestWord.length < arrayOfStrings[i].length) {
+			 longestWord = arrayOfStrings[i];
+		}
+	}
+	return longestWord;
+}
+
+var testArray = ["long", "longest", "etetetetetetettetetetetetetetetetetettetetetete", "etes,th.stphsoennet", "3394949494933"];
+var anotherArray = ["1", "22", "999999999", "666666", "4444", "7777777", "55555", "333", "88888888"]
+
+console.log(returnLongest(testArray));
+console.log(returnLongest(anotherArray))
