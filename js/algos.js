dc2588b3262864869e098976982c90abc2c416f1
Sam Hopkins <hopkins.571@gmail.com>
2016-09-17 23:46:48 -0500

    pseudocode returnLongest function

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// FUNCTION: find the longest phrase
+// INPUT PARAMETERS: an array of multiple strings
+// OUTPUT: variable longestWord - the string that was the longest of all in the input array
+// create new variable longestWord - set it equal to index 0 of the input array
+// starting at index 1, iterate through the remaining indices of the array:
+// IF the length of the variable longestWord is LESS THAN the length of the current index of the array
+// THEN replace the value of longestWord with the current index of the array
+// RETURN longestWord
