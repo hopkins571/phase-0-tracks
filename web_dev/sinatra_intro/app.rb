8cdeb68856620697f56f3c111f188a72e861c282
Sam Hopkins <hopkins.571@gmail.com>
2016-10-02 15:10:24 -0500

    add contact route

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,9 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# write a GET route that displays an address
+
+get '/contact' do
+  "Visit us at 123 Main Street, Springfield, OH 44140"
+end
